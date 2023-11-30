123eba278daa4d48685b5c662eb070c94d01decd
Benedict Cairns <bcairns21@gmail.com>
2023-11-30 15:10:16 +0000

    Correct addressing (again)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'aoc-scaffold-2023',
+  base: 'aoc-2023-react',
   plugins: [react()]
 })
