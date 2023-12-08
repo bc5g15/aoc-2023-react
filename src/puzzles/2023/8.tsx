c398e99a648f5ca589bd60c42c5b1ee5583ee2b6
Benedict Cairns <bcairns21@gmail.com>
2023-12-08 10:45:10 +0000

    Part 2

--- src/puzzles/2023/8.tsx
+++ src/puzzles/2023/8.tsx
@@ -1,4 +1,34 @@
 import { PuzzleForm, Solution } from "../PuzzleForm"
+import { leastCommonMultiple } from "../helpers";
+
+const part2 = (directions: string, maps: Map<string, [string, string]>) => {
+    const nodes = [...maps.keys()].filter(v => v[2] === 'A');
+
+    const stepCounts = nodes.map(n => {
+        let current = n;
+        let steps = 0;
+        while (current[2] !== 'Z') {
+            const direction = directions[steps % directions.length] === 'L' ? 0 : 1;
+            current = maps.get(current)![direction]
+            steps++
+        }
+
+        // It is only by the mercy of the puzzle that I didn't have to do an offset based on loop time!
+        // let seenAgain = false;
+        // let next = current;
+        // let loopTime = 0;
+        // while (!seenAgain) {
+        //     const direction = directions[(loopTime+steps) % directions.length] === 'L' ? 0 : 1;
+        //     next = maps.get(next)![direction]
+        //     if (next === current) seenAgain = true;
+        //     loopTime++
+        // }
+
+        return steps;
+    })
+
+    return leastCommonMultiple(...stepCounts);
+}
 
 const solve: Solution = (input) => {
     const lines = input.split('\n');
@@ -21,11 +51,15 @@ const solve: Solution = (input) => {
         steps++
     }
 
+    const wholeTripTime = part2(directions, directionMap);
+
+
     return (<>
         <div>
             {steps}
         </div>
         <div>
+            {wholeTripTime}
         </div>
     </>)
 }
--- src/puzzles/helpers.tsx
+++ src/puzzles/helpers.tsx
@@ -1,2 +1,15 @@
 export const sum = (arr: number[]) => arr.reduce((acc, cur) => acc+cur, 0);
 
+const gcd = (a: number, b: number) => {
+    for (let temp = b; b !== 0;) {
+        b = a % b;
+        a = temp;
+        temp = b;
+    } 
+    return a;
+}
+
+export const leastCommonMultiple = (...numbers: number[]) => {
+    const _lcm = (x: number, y: number) => (x * y) / gcd(x, y);
+    return numbers.reduce((acc, cur) => _lcm(acc, cur));
+}
