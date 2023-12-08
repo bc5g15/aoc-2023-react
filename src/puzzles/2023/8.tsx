77d6076fca7f4edfe59c92468c057fd1dcd7ff42
Benedict Cairns <bcairns21@gmail.com>
2023-12-08 09:30:39 +0000

    Part 1

--- src/puzzles/2023/8.tsx
+++ src/puzzles/2023/8.tsx
@@ -1,9 +1,29 @@
 import { PuzzleForm, Solution } from "../PuzzleForm"
 
 const solve: Solution = (input) => {
+    const lines = input.split('\n');
+    const directions = lines[0];
+
+    const directionMap: Map<string, [string, string]> = new Map();
+
+    for (let i = 2; i < lines.length; i++) {
+        const line = lines[i];
+        const matches = line.match(/(...) = \((...), (...)\)/u)!;
+        directionMap.set(matches[1], [matches[2], matches[3]])
+    }
+
+    // Navigate
+    let current = 'AAA';
+    let steps = 0;
+    while (current !== 'ZZZ') {
+        const direction = directions[steps % directions.length] === 'L' ? 0 : 1;
+        current = directionMap.get(current)![direction]
+        steps++
+    }
+
     return (<>
         <div>
-            {input}
+            {steps}
         </div>
         <div>
         </div>
