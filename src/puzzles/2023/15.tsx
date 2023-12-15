6f167adfbf097c3cc997608045a769de08d14fc7
Benedict Cairns <bcairns21@gmail.com>
2023-12-15 10:29:11 +0000

    Part 2

--- src/puzzles/2023/15.tsx
+++ src/puzzles/2023/15.tsx
@@ -1,7 +1,7 @@
 import { PuzzleForm, Solution } from "../PuzzleForm"
 import { sum } from "../helpers";
 
-const process = (code: string) => {
+const hash = (code: string) => {
     let value = 0;
     for (let i = 0; i < code.length; i++) {
         value = ((value + code.charCodeAt(i)) * 17) % 256
@@ -9,16 +9,50 @@ const process = (code: string) => {
     return value;
 }
 
+type Lens = {
+    label: string;
+    value: number;
+}
+const part2 = (codes: string[]) => {
+    const boxes: Lens[][] = (new Array(256)).fill(0).map(() => []);
+
+    codes.forEach(code => {
+        const [ label ] = code.match(/\w+/u)!;
+        const index = hash(label);
+        
+        if (code.includes('-')) {
+            boxes[index] = boxes[index].filter(s => s.label !== label)
+        }
+
+        if (code.includes('=')) {
+            const [ value ] = code.match(/\d+/u)!;
+            const arr = boxes[index];
+            const lensIndex = arr.findIndex(v => v.label === label);
+
+            if (lensIndex === -1) {
+                arr.push({ label, value: parseInt(value) })
+            } else {
+                arr[lensIndex].value = parseInt(value);
+            }
+        }
+    });
+
+    const power = sum(boxes.map((b, bi) => sum(b.map((l, li) => (bi+1) * (li+1) * l.value))));
+
+    return power;
+}
+
 const solve: Solution = (input) => {
 
     const codes = input.split(',');
-    const values = codes.map(process);
+    const values = codes.map(hash);
 
     return (<>
         <div>
             {sum(values)}
         </div>
         <div>
+            {part2(codes)}
         </div>
     </>)
 }
