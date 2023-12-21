6d01df48f705ec4982625847f4ab61404ede4501
Benedict Cairns <bcairns21@gmail.com>
2023-12-21 10:08:31 +0000

    Inefficient attempt

--- src/puzzles/2023/21.tsx
+++ src/puzzles/2023/21.tsx
@@ -22,6 +22,11 @@ const DELTAS: [number, number][] = [
     [0, -1]
 ];
 
+const negativeMod = (value: number, mod: number) => {
+    const n = value % mod;
+    return n < 0 ? mod + n : n; 
+}
+
 const walk = (maze: number[][]) => {
     const positions: Set<string> = new Set();
     const [sx, sy] = findStart(maze);
@@ -50,19 +55,53 @@ const walk = (maze: number[][]) => {
         })
     }
 
-    console.log(positions);
+    return positions.size;
+}
+
+const walkInfinite = (maze: number[][]) => {
+    const positions: Set<string> = new Set();
+    const height = maze.length;
+    const width = maze[0].length;
+    const [sx, sy] = findStart(maze);
+    const nodes: [number, number, number][] = [[sx, sy, 0]];
+    const visited: Set<string> = new Set();
+
+    while (nodes.length > 0) {
+        const node = nodes.shift()!;
+        if (visited.has(node.join(','))) {
+            continue;
+        }
+
+        visited.add(node.join(','));
+        const [x, y, steps] = node;
+
+        if (steps === 5000) {
+            positions.add([x,y].join(','));
+            continue;
+        }
+
+        DELTAS.forEach(([dx, dy]) => {
+            const c = maze[negativeMod(y+dy, height)][negativeMod(x+dx, width)];
+            if (c && (c===PLOT || c===START)) {
+                nodes.push([x+dx, y+dy, steps+1]);
+            }
+        })
+    }
+
     return positions.size;
 }
 
 const solve: Solution = (input) => {
     const maze = input.split('\n').map(l => l.split('').map(c => c.charCodeAt(0)));
     const part1 = walk(maze);
+    const part2 = walkInfinite(maze);
 
     return (<>
         <div>
             {part1}
         </div>
         <div>
+            {part2}
         </div>
     </>)
 }
